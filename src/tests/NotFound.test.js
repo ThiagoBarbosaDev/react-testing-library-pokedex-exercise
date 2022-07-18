31688d9eac1e3ed4198288bc48ad4cae23bb212b
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-07-18 17:56:35 -0300

    feat: requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
-// import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../helpers/renderWithRouter';
-import App from '../App';
+import { NotFound } from '../pages';
 
-describe('commit inicial', () => {
-  beforeAll(() => renderWithRouter(<App />));
-
-  it('commit inicial', () => {});
+describe('Requisito 04 - Teste o componente FavoritePokemons', () => {
+  it('deve conter um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundHeader = screen
+      .getByRole('heading', { level: 2, name: /Page requested not found/ });
+    expect(notFoundHeader).toBeInTheDocument();
+  });
+  it('deve renderizar a imagem correta.', () => {
+    renderWithRouter(<NotFound />);
+    const imagemSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imagem = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(imagem.src).toBe(imagemSrc);
+  });
 });
